c0dc7f52fd9028b87bd510e6f91b45da0d0fa578
alunos <alunos@MOCLIi1105.animaedu.intranet>
2018-03-07 19:33:30 -0300

    Exercicio 4

---
+++ Exercicios_Lista1de2/src/vetores/Ex4.java
@@ -0,0 +1,41 @@
+package vetores;
+
+public class Ex4 {
+
+	public static void main(String[] args) {
+		char vetor[] = new char[20];
+		
+		int c = 0;
+		char a = ' ';
+		int resto = 0;
+		
+		for(int i = 65; i < 91; i++)
+		{
+			resto = i%2;
+			
+			if(resto == 0)
+			{
+				i = i - 1;
+				a = (char)i;
+				vetor[c] = a;
+				c = c + 1;
+				i = i + 1;
+			}
+			
+			if(resto != 0)
+			{
+				i = i + 1;
+				a = (char)i;
+				vetor[c] = a;
+				c = c + 1;
+				i = i - 1;
+			}
+		}
+		
+		for(int i = 0; i < 26; i++)
+		{
+		System.out.println(vetor[i]);
+		}
+	}
+
+}
