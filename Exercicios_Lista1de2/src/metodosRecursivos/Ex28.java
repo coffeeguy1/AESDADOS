39a41b221891606268f72ef7192e17362d8cf94d
alunos <alunos@MOCLIi1105.animaedu.intranet>
2018-03-16 21:11:24 -0300

    Exercicio 28 feito

--- Exercicios_Lista1de2/src/metodosRecursivos/Ex28.java
+++ Exercicios_Lista1de2/src/metodosRecursivos/Ex28.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Ex28 {
 	
-	public static int somaMult(int soma, int mult)
+	public static int somaVezes(int soma, int vezes)
 	{
-		if(mult == soma * mult) return mult;
-		else return somaMult(soma, mult + mult);
+		if(vezes == 1) return soma;
+		else return soma + somaVezes(soma, vezes - 1);
 	}
 		
 	public static void main(String[] args) {
@@ -16,14 +16,14 @@ public class Ex28 {
 		do 
 		{
 		Scanner leitura = new Scanner(System.in);
-		System.out.println("Entre com a soma: ");
+		System.out.println("Entre com o valor a ser somado: ");
 		int soma = leitura.nextInt();
-		System.out.println("Entre com o valor a ser multiplicado");
-		int mult = leitura.nextInt();
+		System.out.println("Entre com a quantidade de vezes");
+		int vezes = leitura.nextInt();
 		if(soma >= 0) {
-			if(mult >= 0)
+			if(vezes >= 0)
 				{
-				 System.out.println(somaMult(soma, mult));
+				 System.out.println(somaVezes(soma, vezes));
 				 valor = 1;
 				}
 		}else {
