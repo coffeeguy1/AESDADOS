7be5a7c3bc1ee5d1c222cdcf4badb309032798f7
Rafael <Rafael@192.168.15.11>
2018-03-27 23:48:39 -0300

    41 Feito

--- Exercicios_Lista1de2/src/buscaLinearRecursiva/Ex41.java
+++ Exercicios_Lista1de2/src/buscaLinearRecursiva/Ex41.java
@@ -3,6 +3,7 @@ package buscaLinearRecursiva;
 import javax.swing.JOptionPane;
 
 public class Ex41 {
+	static double indice = 0;
 	public static void main(String[] args) {
 		double iVetor[] = new double[10];
 		double iIndice;
@@ -10,7 +11,7 @@ public class Ex41 {
 		
 		for(int i = 0; i<iVetor.length; i++)
 		{
-			iVetor[i] = Double.valueOf(JOptionPane.showInputDialog("Digite o valor do indice" + i));
+			iVetor[i] = Double.valueOf(JOptionPane.showInputDialog("Digite o valor do indice " + i));
 		}
 		iK = Integer.parseInt(JOptionPane.showInputDialog("Digite a chave de busca"));
 		
@@ -19,7 +20,7 @@ public class Ex41 {
 		String sTitle = "Busca Linear Recursiva - Saida";
 		String sIO = "O número " + iK;
 		
-		if(iIndice < iVetor.length)
+		if(iIndice < iVetor.length-1)
 			sIO += " está localizado no indice " + iIndice + " do vetor.";
 		else sIO += " não esta localizado no vetor.";
 		JOptionPane.showMessageDialog(null, sIO, sTitle, JOptionPane.INFORMATION_MESSAGE);
@@ -29,10 +30,16 @@ public class Ex41 {
 	
 	public static double busca(double iV[], int iI, double iK)
 	{
-		if(iI <= (iV.length-1) && (iK != iV[iI]))
-		{
+		if(iI <= (iV.length-1))
+		{	
+			if((iK == iV[iI]))
+			{
+				indice = iI;
+			}else if(iI == iV.length-1){
+				return iI;
+			}
 			return busca(iV, iI+1,iK);
 		}
-		return iI;
+		return indice;
 	}
 }
