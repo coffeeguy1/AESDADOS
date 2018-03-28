2688fe6aaf5009573ba946de95ce987717995f8a
Rafael <Rafael@192.168.15.11>
2018-03-28 00:10:34 -0300

    Em vias ex42

--- Exercicios_Lista1de2/src/buscaBinaria/Ex42.java
+++ Exercicios_Lista1de2/src/buscaBinaria/Ex42.java
@@ -1,7 +1,29 @@
 package buscaBinaria;
 
 public class Ex42 {
-
+	
+	public static int busca_binaria(int iVet[], int iK)
+	{
+		int iBaixo, iAlto, iMeio;
+		
+		iBaixo = 0;
+		iAlto=iVet.length-1;
+		while(iBaixo <= iAlto)
+		{
+			iMeio=(iBaixo + iAlto)/2;
+			
+			if(iK < iVet[iMeio])
+				{
+					iAlto = iMeio-1;
+				}
+			else if(iK > iVet[iMeio]) {
+				iBaixo = iMeio+1;
+			}
+			else return iMeio;
+		}
+		return -1;
+	}
+	
 	public static void main(String[] args) {
 		
 
