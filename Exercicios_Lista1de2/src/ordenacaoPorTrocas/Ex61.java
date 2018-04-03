c015dc3e39691ee6e2cc9473912a43c07641716f
Rafael <Rafael@192.168.15.12>
2018-04-02 21:50:40 -0300

    61

---
+++ Exercicios_Lista1de2/src/ordenacaoPorTrocas/Ex61.java
@@ -0,0 +1,30 @@
+package ordenacaoPorTrocas;
+
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
+
+public class Ex61 {
+	public static void bolha(double vet[])
+    {
+       for(int i=1; i<= vet.length-1; i++)
+          for(int j=0; j<vet.length-1; j++)
+             if(vet[j]<vet[j+1])
+             {
+                double aux = vet[j];
+                vet[j] = vet[j+1];
+                vet[j+1] = aux;
+             }
+    }
+		
+	public static void main(String[] args) {
+		double vetor[] = new double[20];
+		for(int i =0; i < 20; i++) {
+			vetor[i] = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de " + i));
+		}
+		
+		bolha(vetor);
+		System.out.println(Arrays.toString(vetor));
+	}
+
+}
