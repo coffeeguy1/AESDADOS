f8b17659324f267b4741f552983badc543399f7e
alunos <alunos@MOCLIi1105.animaedu.intranet>
2018-04-18 20:30:48 -0300

    MDC

---
+++ Exercicios_Lista1de2/src/metodosRecursivos/Ex26.java
@@ -0,0 +1,14 @@
+package metodosRecursivos;
+
+import javax.swing.JOptionPane;
+
+public class Ex26 {
+	
+	public static void main(String[] args) {
+		int n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro valor: "));
+		int n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo valor: "));
+		
+		//MDC(180, 240, 270) = 2 x 3 x 5 = 30.
+	}
+
+}
