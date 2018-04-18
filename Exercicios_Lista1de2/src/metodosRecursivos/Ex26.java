3d6fd0f49af9da282ce60c82d8662ea0e94f5863
alunos <alunos@MOCLIi1105.animaedu.intranet>
2018-04-18 20:48:54 -0300

    20:48

--- Exercicios_Lista1de2/src/metodosRecursivos/Ex26.java
+++ Exercicios_Lista1de2/src/metodosRecursivos/Ex26.java
@@ -1,14 +1,29 @@
 package metodosRecursivos;
 
 import javax.swing.JOptionPane;
-
 public class Ex26 {
+	
+	public static int MDC(int n1, int n2, int multDiv)
+	{
+		multDiv = 0;
+		int calc = 0;  /// essa variavel será responsável por dividir os valores.
+		
+		if(n1 == 1 && n2 == 1) return multDiv;  
+		else if(calc % 2 == 0 ){
+				if()
+			return MDC(n1/calc, n2/calc, multDiv);
+		}
+		return -1;
+	}
+	
 	
 	public static void main(String[] args) {
 		int n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro valor: "));
 		int n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo valor: "));
 		
 		//MDC(180, 240, 270) = 2 x 3 x 5 = 30.
+		
+		
 	}
 
 }
