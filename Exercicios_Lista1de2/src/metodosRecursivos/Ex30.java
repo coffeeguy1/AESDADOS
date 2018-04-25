e4959d18b22285a0ba41006269d3a0aa2791faf3
alunos <alunos@MOCLIi1105.animaedu.intranet>
2018-04-25 19:34:31 -0300

    fila pilha

--- Exercicios_Lista1de2/src/metodosRecursivos/Ex30.java
+++ Exercicios_Lista1de2/src/metodosRecursivos/Ex30.java
@@ -3,15 +3,31 @@ package metodosRecursivos;
 import javax.swing.JOptionPane;
 
 public class Ex30 {
-	public static int termo(int n1, int vezes)
+	public static double termo(double n1, double n2, double vezes)
 	{
 		if(vezes == 0) return n1;
-		else return n1 + termo(1/n1, vezes - 1);
+		else { 
+			n2 = Math.pow(n2, 2);
+			return termo(n1 + (1/n2), n2, vezes - 1);
+		}
 	}
 	public static void main(String[] args) {
-		int n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro valor: "));
-		int vezes = n1;
-		System.out.println(termo(n1, vezes));
+		double n1 = 1;
+		double n2 = 0;
+		double vezes = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro valor: "));;
+		
+		System.out.println(termo(n1, n2, vezes));
 	}
 
 }
+/*
+ * termo(n1 + (1/n2), n2, vezes -1)
+1)
+	termo(1 + (1/2), 2, 4 - 1)
+2)
+	termo(1 + (1/4), 2, 4 - 1)
+3)
+	termo(1 + (1/2), 2, 4 - 1)
+4)
+	termo(1 + (1/2), 2, 4 - 1)
+*/
